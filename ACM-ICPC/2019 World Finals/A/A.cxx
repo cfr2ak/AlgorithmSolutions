39835b4b41f525cea243d975f44fb8584ccbcafb
cfr2ak <cfr2ak@gmail.com>
2019-11-18 20:51:33 +0900

    Load input with vector of the tuple

--- ACM-ICPC/2019 World Finals/A/A.cxx
+++ ACM-ICPC/2019 World Finals/A/A.cxx
@@ -1,47 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
     int n = 0;
+    vector<tuple<int, int>> back ;
+    vector<tuple<int, int>> front;
+
+    cin >> n;
+    getInput(&back, &front, n);
+
+    sort(back.begin(), back.end());
+    sort(front.begin(), front.end());
+
+
+    return 0;
+}
+
+void getInput(vector<tuple<int, int>> *back, vector<tuple<int, int>> *front, int n)
+{
     vector<int> priceBack = {};
     vector<int> priceFront = {};
     vector<int> heightBack = {};
     vector<int> heightFront = {};
 
-    cin >> n;
-    getInput(&priceBack, &priceFront, &heightBack, &heightFront, n);
-
-    return 0;
-}
-
-void getInput(vector<int> *priceBack, vector<int> *priceFront, vector<int> *heightBack, vector<int> *heightFront, int n)
-{
-    for (auto j = 0; j < n; ++j)
+    for (auto i = 0; i < n; ++i)
     {
         auto temp = 0;
         cin >> temp;
-        priceBack->push_back(temp);
+        priceBack.push_back(temp);
     }
-    for (auto j = 0; j < n; ++j)
+    for (auto i = 0; i < n; ++i)
     {
         auto temp = 0;
         cin >> temp;
-        priceBack->push_back(temp);
+        heightBack.push_back(temp);
     }
-    for (auto j = 0; j < n; ++j)
+    for (auto i = 0; i < n; ++i)
     {
-        auto temp = 0;
-        cin >> temp;
-        priceBack->push_back(temp);
-    }
-    for (auto j = 0; j < n; ++j)
-    {
-        auto temp = 0;
-        cin >> temp;
-        priceBack->push_back(temp);
+        back->push_back(tuple<int, int>(priceBack[i], heightBack[i]));
     }
 
+    for (auto i = 0; i < n; ++i)
+    {       
+        auto temp = 0;
+        cin >> temp;
+        priceFront.push_back(temp);
+    }
+    for (auto i = 0; i < n; ++i)
+    {
+        auto temp = 0;
+        cin >> temp;
+        heightFront.push_back(temp);
+    }
+    for (auto i = 0; i < n; ++i)
+    {
+        front->push_back(tuple<int, int>(priceFront[i], heightFront[i]));
+    }
+    return;
 }
